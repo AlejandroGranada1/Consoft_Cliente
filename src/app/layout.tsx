db96cd3cff02a47f1ed43e30a6a4cb04a90abb5c
Alejandro Granada Muñoz <alejandrogranadauio@gmail.com>
2025-09-23 09:05:38 -0500

    Cambios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ export const metadata: Metadata = {
   description: "Aplicación Web desarrollada para Confort & Estilo",
 };
 
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
