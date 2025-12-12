2242a73b7201154762a5c5f8492bb7b5ec3f82df
AlejandroGranada1 <alejandrogranadauio@gmail.com>
2025-12-11 21:24:43 -0500

    Add middleware for token authentication and redirect to login

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+	const token = req.cookies.get('token')?.value;
+
+	if (!token) {
+		const loginUrl = new URL('/client/auth/login', req.url);
+		return NextResponse.redirect(loginUrl);
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ['/admin/:path*'],
+};
